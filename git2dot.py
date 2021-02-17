95a5505747c577f499cafd6a07899ac3b01e897e
Lars Kellogg-Stedman <lars@oddbit.com>
2021-02-17 16:49:10 -0500

    make nicer git graphs
    this integrates information from the graphviz docs,
    research from [1], and examples from [2].
    
    [1]: https://stackoverflow.com/questions/66247706/force-linear-layout-of-nodes-in-graphviz/66249891#66249891
    [2]: https://correl.phoenixinquis.net/2015/07/12/git-graphs.html

--- git2dot.py
+++ git2dot.py
@@ -1,6 +1,5 @@
 import click
 import git
-import json
 
 from graphviz import Digraph
 
@@ -10,7 +9,12 @@ from graphviz import Digraph
 @click.option('-r', '--render', is_flag=True)
 @click.option('-v', '--view', is_flag=True)
 @click.option('-f', '--format', default='svg')
-def main(output, render, view, format):
+@click.option('-m', '--use-message', is_flag=True)
+@click.option('--remote/--no-remote', 'flag_remote')
+@click.option('--tags/--no-tags', 'flag_tags')
+@click.option('--rankdir', default='TB')
+def main(output, render, view, format, flag_remote, flag_tags,
+         rankdir, use_message):
     if (render or view) and not output:
         raise click.ClickException('--render and --view require --output')
 
@@ -18,24 +22,72 @@ def main(output, render, view, format):
     seen = set()
 
     graph = Digraph(name='git', format='svg')
+    graph.attr(rankdir=rankdir)
+
+    commits = Digraph()
+    commits.node_attr['group'] = 'commits'
+
+    heads = Digraph()
+    heads.node_attr = dict(
+        group='heads', shape='box', color='black', style='filled', fontcolor='white',
+    )
+
+    remote_heads = Digraph()
+    remote_heads.node_attr = dict(
+        group='remote_heads', shape='box', color='grey', style='filled', fontcolor='white',
+    )
+
+    tags = Digraph()
+    tags.node_attr = dict(
+        group='tags', shape='cds', color='green', style='filled', fontcolor='black',
+    )
+
+    branch_links = []
+    commit_links = []
+
     for head in repo.heads:
-        graph.node(head.name,
-                   shape='box', color='black', style='filled',
-                   fontcolor='white')
-        graph.edge(head.name, head.commit.hexsha[:10], style='dashed')
+        heads.node(head.name)
+        branch_links.append((head.name, head.commit.hexsha[:10]))
 
         for commit in [head.commit] + list(head.commit.traverse()):
             if commit in seen:
                 continue
 
-            graph.node(name=commit.hexsha[:10],
-                       tooltip=commit.message.splitlines()[0])
+            args = dict(name=commit.hexsha[:10],
+                        tooltip=commit.message.splitlines()[0])
+
+            if use_message:
+                msg = commit.message.splitlines()[0]
+                args['label'] = msg
+            commits.node(**args)
             seen.add(commit)
 
             for parent in commit.parents:
                 if (commit, parent) in seen:
                     continue
-                graph.edge(commit.hexsha[:10], parent.hexsha[:10])
+                commit_links.append((commit.hexsha[:10], parent.hexsha[:10]))
+
+    graph.subgraph(commits)
+    graph.subgraph(heads)
+
+    if flag_remote:
+        for remote in repo.remotes:
+            for ref in remote.refs:
+                remote_heads.node(ref.name)
+                branch_links.append((ref.name, ref.commit.hexsha[:10]))
+        graph.subgraph(remote_heads)
+
+    if flag_tags:
+        for tag in repo.tags:
+            tags.node(tag.name)
+            branch_links.append((tag.name, f'{tag.commit.hexsha[:10]}:s'))
+        graph.subgraph(tags)
+
+    with graph.subgraph() as sub:
+        sub.edge_attr = dict(style='dashed')
+        sub.edges(branch_links)
+
+    graph.edges(commit_links)
 
     if not output:
         print(graph.source)
